d281a6bf7756f197b706e4c63fcffb549f618789
poonasq257 <poonasp257@naver.com>
2019-01-31 20:48:36 +0900

    Init
    팀 개발 언리얼 프로젝트 생성

---
+++ ProjectNero/Source/ProjectNero/ProjectNeroGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectNeroGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectNeroGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectNeroGameMode();
+};
+
+
+
