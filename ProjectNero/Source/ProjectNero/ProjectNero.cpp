d900ff42688e0e278518d2add49e756c6fa5d65f
poonasq257 <poonasp257@naver.com>
2019-02-12 11:38:38 +0900

    Delete
    .

--- ProjectNero/Source/ProjectNero/ProjectNero.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "ProjectNero.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectNero, "ProjectNero" );
- 
